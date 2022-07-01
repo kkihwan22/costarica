9c0100dfd711c4063999399b8f0431ffe73554fb
kkihwan22 <jinkh97@gmail.com>
2022-07-01 15:33:08 +0900

    프로젝트 생성

---
+++ README.md
@@ -0,0 +1,13 @@
+코스플레이어를 위한 맞춤 플랫폼
+
+개요 
+
+WEB 서비스를 담당하는 프로젝트. REST / PAGE 서비스 제공 한다.
+
+
+개발환경
+
+- Java 11 / Spring Boot / Gradle
+- 인프라 : MySQL, REDIS, 
+
+
---
+++ src/main/java/me/kkihwan/web/CostaricaApplication.java
@@ -0,0 +1,13 @@
+package me.kkihwan.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostaricaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostaricaApplication.class, args);
+    }
+
+}
