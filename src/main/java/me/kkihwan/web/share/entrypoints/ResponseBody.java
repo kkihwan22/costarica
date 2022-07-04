a15aeed8bdc1894c152e6136e59dffdfed506e5d
kihwan <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>
2022-07-05 00:33:09 +0900

    [](feature) ResponseBodyFactory 클래스에 대한 테스트 코드 추가

--- src/main/java/me/kkihwan/web/share/entrypoints/ResponseBody.java
+++ src/main/java/me/kkihwan/web/share/entrypoints/ResponseBody.java
@@ -3,6 +3,8 @@ package me.kkihwan.web.share.entrypoints;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @Getter @ToString
 public class ResponseBody<T> {
     private int code;
@@ -15,5 +17,18 @@ public class ResponseBody<T> {
         this.message = message;
         this.traceId = traceId;
         this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ResponseBody<?> that = (ResponseBody<?>) o;
+        return code == that.code && Objects.equals(message, that.message) && Objects.equals(traceId, that.traceId) && Objects.equals(data, that.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, message, traceId, data);
     }
 }
---
+++ src/test/java/me/kkihwan/web/share/entrypoints/ResponseBodyFactoryTest.java
@@ -0,0 +1,23 @@
+package me.kkihwan.web.share.entrypoints;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ResponseBodyFactoryTest {
+    private final Logger LOG = LoggerFactory.getLogger(ResponseBodyFactoryTest.class);
+    private final Map<String, String> data = Map.of("key", "value");
+
+    @Test
+    @DisplayName("성공했을 경우의 응답 모델 생성 테스트입니다.")
+    void successResponseBody() {
+        ResponseBody<Map<String,String>> expect = new ResponseBody<>(0, "OK", "", data);
+        ResponseBody<Map<String, String>> body = ResponseBodyFactory.success(data);
+        assertEquals(expect, body);
+    }
+}
