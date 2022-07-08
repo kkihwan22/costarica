57c931de3cdfc40da9d12087518a8ad12a998d0f
kkihwan22 <jinkh97>
2022-07-08 13:27:01 +0900

    [] (config) 로깅 관련 설정 추가
    - logback 설정 추가
    - MdcFilter 로 traceId 출력할 수 있도록 추가

---
+++ src/main/java/me/kkihwan/web/config/filter/MdcFilter.java
@@ -0,0 +1,34 @@
+package me.kkihwan.web.config.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import java.io.IOException;
+import java.util.UUID;
+
+@WebFilter
+public class MdcFilter extends HttpFilter {
+    private final static Logger LOG = LoggerFactory.getLogger(MdcFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            MDC.put("traceId", generateTraceId());
+            chain.doFilter(request, response);
+        } finally {
+            LOG.debug(" MDC clear.");
+            MDC.clear();
+        }
+    }
+
+    private String generateTraceId() {
+        return UUID.randomUUID().toString();
+    }
+}
