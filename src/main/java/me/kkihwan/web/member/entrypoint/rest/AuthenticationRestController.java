32efc3347de3b04db09f23fdb7f4ecf52372cab3
kkihwan22 <jinkh97@gmail.com>
2022-10-17 01:06:18 +0900

    AccessToken 재발행 Endpoint 추가

--- src/main/java/me/kkihwan/web/member/entrypoint/rest/AuthenticationRestController.java
+++ src/main/java/me/kkihwan/web/member/entrypoint/rest/AuthenticationRestController.java
@@ -1,13 +1,19 @@
 package me.kkihwan.web.member.entrypoint.rest;
 
-import lombok.*;
-import org.slf4j.*;
+import lombok.RequiredArgsConstructor;
+import me.kkihwan.web.member.application.AuthenticationService;
+import me.kkihwan.web.shared.entrypoint.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class AuthenticationRestController {
     private final Logger log = LoggerFactory.getLogger(AuthenticationRestController.class);
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/public/v1/login")
     public void login() {
@@ -15,7 +21,9 @@ public class AuthenticationRestController {
     }
 
     @GetMapping("/public/v1/token/refresh")
-    public void refreshToken() {
-
+    public ResponseEntity<Body<SimpleBodyData<String>>> refreshToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        log.debug("[ Authorization Header ] token : {} ", token);
+        String newAccessToken = authenticationService.issueAccessTokenByRefreshToken(token);
+        return ResponseEntity.ok(BodyFactory.success(new SimpleBodyData(newAccessToken)));
     }
 }
